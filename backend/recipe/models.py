8cd2a90d55ff38bce43ce9dd9f26e8b8d72ea160
Jacex <jacek.chabielski@gmail.com>
2022-12-17 10:20:08 +0100

    stworzenie osobnego modelu dla quantity produktu w przepisie

--- backend/recipe/models.py
+++ backend/recipe/models.py
@@ -6,11 +6,13 @@ from recipe_category.models import Recipe_category
 from io import BytesIO
 from PIL import Image
 from django.core.files import File
+from django.core.validators import MaxValueValidator, MinValueValidator
+
 
 class Recipe(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank = True)
-    product = models.ManyToManyField(Product, related_name='product', blank=True)
+    #product = models.ManyToManyField(Product, related_name='product', blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     slug = models.SlugField(max_length = 255, blank = True)
     image = models.ImageField(upload_to = 'uploads/recipe/', blank = True, null = True)
@@ -57,3 +59,31 @@ class Recipe(models.Model):
         img.save(thumb_io, 'PNG', quality = 85)
         thumbnail = File(thumb_io, name = image.name)
         return thumbnail
+
+    def add_product(self, product, quantity= 1):
+        new_recipe_product = RecipeProduct.objects.get_or_create(product_id= product.id, recipe_id= self.id)
+        new_recipe_product.quantity += quantity
+        return new_recipe_product
+    
+    def update_product(self, product, quantity):
+        recipe_product = RecipeProduct.get(product_id= product.id, recipe_id= self.id)
+        if not recipe_product: return None 
+        recipe_product.quantity = quantity
+        return recipe_product
+    
+    def delete_product(self, product_id):
+        recipe_product = RecipeProduct.get(product_id= product_id, recipe_id= self.id)
+        if not recipe_product is None: recipe_product.delete()
+
+class RecipeProduct(models.Model):
+    product_id = models.ForeignKey(Product, on_delete=CASCADE, related_name='product_id', blank=False)
+    recipe_id = models.ForeignKey(Recipe,  on_delete=CASCADE, related_name='recipe_id', blank=False)
+    quantity = models.IntegerField(default=0, validators=[MinValueValidator(1)])
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-created',)
+    
+    def __str__(self):
+        return self.product_id.name + " of " + self.recipe_id.name
+    
